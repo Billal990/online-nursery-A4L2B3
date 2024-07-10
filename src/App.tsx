09147712f3362bd657233fdd1653a6b34a501117
billal990 <billalbelal621@gmail.com>
2024-07-10 09:54:55 +0600

    completed project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">App Here</h1>
+      <Button>Click On Me!</Button>
+    </div>
+  )
+}
